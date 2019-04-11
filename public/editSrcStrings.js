6b83848aea0b11175b4e24de21aed329dfd05247
Rick Conrad <richardjconrad@gmail.com>
2019-04-11 16:41:52 +0100

    Improve source editing experience. Add edit string and delete functionality

--- public/editSrcStrings.js
+++ public/editSrcStrings.js
@@ -1,57 +1,149 @@
-function onMergeCheck(id) {
+/*
+    Merge Next Button
+*/
+function mergeNext(id) {
   var row = document.getElementById("Row-" + id);
   var nextRow = row.nextElementSibling;
 
-  // Set the draft text in the text input
-  var draftText = getTextFromRow(row) + getTextFromRow(nextRow);
-  row
-    .getElementsByClassName("merge-text-input")[0]
-    .getElementsByTagName("input")[0].value = draftText;
+  // Show the draft text
+  var draftTextHtml =
+    "<span>" +
+    getTextFromRow(row) +
+    "</span><span>" +
+    getTextFromRow(nextRow) +
+    "</span>";
+  row.querySelector("div.text").innerHTML = draftTextHtml;
 
   // Hide and show things
-  var editCell = row.getElementsByClassName("edit")[0];
-  editCell.getElementsByClassName("merge-check")[0].style.display = "none";
-  editCell.getElementsByClassName("merge-text-input")[0].style.display =
-    "block";
-
-  // Uncheck the checkbox
-  row.querySelector("input[type=checkbox]").checked = false;
+  nextRow.style.display = "none";
+  row.querySelector("div.base-buttons").style.display = "none";
+  row.querySelector("div.merge-in-progress").style.display = "block";
 }
 
 function getTextFromRow(row) {
   return row.firstElementChild.firstElementChild.innerText;
 }
 
+/*
+  Cancel Merge Button
+*/
+function cancelMerge(id) {
+  var row = document.getElementById("Row-" + id);
+  var nextRow = row.nextElementSibling;
+  var originalText = row.querySelector("div.text span").innerText.trim();
+  row.querySelector("div.text").innerText = originalText;
+
+  nextRow.style.display = ""; // Default for tr
+  row.querySelector("div.base-buttons").style.display = "block";
+  row.querySelector("div.merge-in-progress").style.display = "none";
+}
+
+/*
+  Add Space Button
+*/
+function addMergeSpace(id) {
+  var row = document.getElementById("Row-" + id);
+  row.querySelector("div.text span").innerText += " ";
+}
+
+/*
+  Confirm Merge Button
+*/
 function confirmMerge(id) {
   var row = document.getElementById("Row-" + id);
   var nextRow = row.nextElementSibling;
   var nextId = nextRow.getAttribute("data-id");
-  var text = row.querySelector("input[type=text]").value;
+  var text = row.querySelector("div.text").innerText;
 
   // Update this row
-  row.querySelector("div.text").innerText = text;
+  row.querySelector("div.base-buttons").style.display = "block";
+  row.querySelector("div.merge-in-progress").style.display = "none";
   row.querySelector("div.merged-note").style.display = "block";
-  row.querySelector("div.merge-check").style.display = "block";
-  row.querySelector("div.merge-text-input").style.display = "none";
 
   // Remove next row
   row.parentNode.removeChild(nextRow);
 
-  // Remove any existing hidden inputs
-  var form = document.getElementById("editSrcStringsForm");
-  removeHiddenInput(form, id);
-  removeHiddenInput(form, nextId);
-
   // Add hidden inputs
-  form.insertAdjacentHTML("afterbegin", hiddenInputHtml(id, text));
-  form.insertAdjacentHTML("afterbegin", hiddenInputHtml(nextId, ""));
+  setHiddenInput(id, text);
+  setHiddenInput(nextId, "");
+}
+
+/*
+  Edit Button
+*/
+function edit(id) {
+  var row = document.getElementById("Row-" + id);
+  var text = row.querySelector("div.text").innerText;
+  row.querySelector("input[type=text]").value = text;
+
+  row.querySelector("div.text").style.display = "none";
+  row.querySelector("div.text-input").style.display = "block";
+  row.querySelector("div.base-buttons").style.display = "none";
+  row.querySelector("div.edit-in-progress").style.display = "block";
+}
+
+/*
+  Save Edit Button
+*/
+function saveEdit(id) {
+  var row = document.getElementById("Row-" + id);
+  var text = row.querySelector("input[type=text]").value;
+  var oldText = row.querySelector("div.text").innerText.trim();
+
+  if (text != oldText) {
+    row.querySelector("div.text").innerText = text;
+    row.querySelector("div.edited-note").style.display = "block";
+    setHiddenInput(id, text);
+  }
+
+  row.querySelector("div.text").style.display = "block";
+  row.querySelector("div.text-input").style.display = "none";
+  row.querySelector("div.base-buttons").style.display = "block";
+  row.querySelector("div.edit-in-progress").style.display = "none";
+}
+
+/*
+  Delete Button
+*/
+function deleteItem(id) {
+  var row = document.getElementById("Row-" + id);
+
+  row.querySelector("div.text").style.textDecoration = "line-through";
+  row.querySelector("div.base-buttons").style.display = "none";
+  row.querySelector("div.delete-in-progress").style.display = "block";
+}
+
+/*
+  Cancel Delete
+*/
+function cancelDelete(id) {
+  var row = document.getElementById("Row-" + id);
+
+  row.querySelector("div.text").style.textDecoration = "initial";
+  row.querySelector("div.base-buttons").style.display = "block";
+  row.querySelector("div.delete-in-progress").style.display = "none";
+}
+
+/*
+  Confirm Delete Button
+*/
+function confirmDelete(id) {
+  var row = document.getElementById("Row-" + id);
+  row.parentElement.removeChild(row);
+  setHiddenInput(id, "");
+}
+
+function setHiddenInput(id, value) {
+  var form = document.getElementById("editSrcStringsForm");
+
+  // Remove existing
+  var input = form.querySelector("input[name='" + id + "']");
+  if (input) form.removeChild(input);
+
+  // Add input
+  form.insertAdjacentHTML("afterbegin", hiddenInputHtml(id, value));
 }
 
 function hiddenInputHtml(id, value) {
   return "<input type='hidden' name='" + id + "' value='" + value + "' />";
-}
-
-function removeHiddenInput(form, id) {
-  var input = form.querySelector("input[name='" + id + "']");
-  if (input) form.removeChild(input);
 }
