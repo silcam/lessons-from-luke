08ca07df06a9eee14688d1654cd4c48ad35e8a90
Rick Conrad <richardjconrad@gmail.com>
2019-08-22 10:03:54 +0100

    Include target language name in name of downloaded .odt files.

--- src/controllers/documentsController.ts
+++ src/controllers/documentsController.ts
@@ -11,11 +11,15 @@ export default function documentsController(app: Express) {
   });
 
   app.get("/documents/translation/:projectId/:lessonFilename", (req, res) => {
-    const lesson = stripOdtExtension(req.params.lessonFilename);
+    const lesson = lessonFromProjectFilename(req.params.lessonFilename);
     const projectId = Storage.projectIdFromString(req.params.projectId);
     const docPath = documentPathForTranslation(projectId, lesson);
     res.sendFile(docPath);
   });
+}
+
+function lessonFromProjectFilename(filename: string) {
+  return stripOdtExtension(filename).replace(/^[^-]+-/, ""); // Remove "Ewondo-" from "Ewondo-Luke-T1-L01"
 }
 
 function stripOdtExtension(filename: string) {
--- test/controllers/documentsController.test.ts
+++ test/controllers/documentsController.test.ts
@@ -34,7 +34,7 @@ test("Generate and download translated doc", async () => {
   expect.assertions(2);
   const agent = await plainAgent();
   const response = await agent.get(
-    "/documents/translation/Pidgin_1555081479425/Luke-Q1-L01.odt"
+    "/documents/translation/Pidgin_1555081479425/Pidgin-Luke-Q1-L01.odt"
   );
   expect(response.status).toBe(200);
   expect(response.type).toEqual("application/vnd.oasis.opendocument.text");
