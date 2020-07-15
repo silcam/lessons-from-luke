54af1474dae1f344ad7e5a0de50ad4df54a89aad
Rick Conrad <richardjconrad@gmail.com>
2020-07-15 15:01:27 -0400

    Deploy from master branch.

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :repo_url, "https://github.com/silcam/lessons-from-luke.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
-set :branch, "refactor"
+# set :branch, "refactor"
 
 # Default deploy_to directory is /var/www/my_app_name
 # set :deploy_to, "/var/www/my_app_name"
