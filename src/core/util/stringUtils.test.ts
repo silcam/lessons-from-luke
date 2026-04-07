35c841a06677f6a9490cbf6888d8775ba98b1bfc
eykd <david@worldsenoughstudios.com>
2026-04-07 14:54:48 -0700

    experiment(utils): add tests for escapeHTML and faded utility functions

--- src/core/util/stringUtils.test.ts
+++ src/core/util/stringUtils.test.ts
@@ -1,7 +1,14 @@
-import { onlyWhitespace } from "./stringUtils";
+import { onlyWhitespace, escapeHTML } from "./stringUtils";
 
 test("onlyWhitespace", () => {
   expect(onlyWhitespace("    \t\n")).toBe(true);
   expect(onlyWhitespace("   a  ")).toBe(false);
   expect(onlyWhitespace("   .  ")).toBe(false);
+});
+
+test("escapeHTML escapes &, <, and > characters", () => {
+  expect(escapeHTML("a & b")).toBe("a &amp; b");
+  expect(escapeHTML("<div>")).toBe("&lt;div&gt;");
+  expect(escapeHTML("a < b > c")).toBe("a &lt; b &gt; c");
+  expect(escapeHTML("no special chars")).toBe("no special chars");
 });
